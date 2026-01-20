203c93c3ef5e4c67d8d0b271a8b62999325fde3d
Abraham <abrahanvicioso@gmail.com>
2026-01-19 22:45:12 -0400

    Añadir gateway API con Ocelot y nuevo proyecto ApiGateway
    Se ha integrado el paquete `Ocelot` en el proyecto de autenticación y se ha creado un nuevo proyecto llamado `ApiGateway` para manejar las solicitudes de API.
    
    Se ha añadido un archivo `ApiGateway.csproj` que define un proyecto web en .NET 9.0, junto con configuraciones de registro en `appsettings.Development.json` y `appsettings.json`.
    
    Además, se ha creado un archivo `ocelot.json` para definir las rutas del gateway API y se ha modificado `Program.cs` para incluir la configuración de Ocelot. También se ha añadido un archivo `launchSettings.json` para las configuraciones de lanzamiento en el entorno de desarrollo.

---
+++ ApiGateway/Program.cs
@@ -0,0 +1,42 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using System.Threading.Tasks;
+
+namespace ApiGateway
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+
+            builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+            builder.Services.AddOcelot();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            await app.UseOcelot();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
