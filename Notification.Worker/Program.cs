834f0b66860404c297e180e2f869a841be0695d7
Abraham <abrahanvicioso@gmail.com>
2026-02-01 09:31:37 -0400

    Añadir nuevos proyectos y configuración a la solución
    Se han añadido varios proyectos al archivo de solución `Barcelo-Integrated-Iot.sln`, incluyendo "Core", "Infrastructure", "Presentation", "Notification.Domain", "Notificacion.Application", "Notification.Kafka" y "Notification.Worker", cada uno con su propia configuración de compilación.
    
    Se han creado nuevos archivos de proyecto (`.csproj`) para "Notificacion.Application", "Notification.Domain", "Notification.Kafka" y "Notification.Worker", configurados para usar el SDK de .NET y dirigidos a la versión `net9.0`, habilitando `ImplicitUsings` y `Nullable`.
    
    Además, se ha añadido un nuevo archivo `Program.cs` en "Notification.Worker" que contiene una clase `Program` con un método `Main` que imprime "Hello, World!" en la consola.

---
+++ Notification.Worker/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Notification.Worker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
