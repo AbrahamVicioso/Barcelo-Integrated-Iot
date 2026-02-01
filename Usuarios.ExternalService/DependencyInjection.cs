e530951c0ded452acfe197b6c95adb3cd9e90396
Abraham <abrahanvicioso@gmail.com>
2026-02-01 13:22:27 -0400

    Añadir configuración para el servicio de autenticación
    Se han realizado cambios en `DependencyInjection.cs` para incluir la configuración de `AuthenticationApiOptions` y se ha actualizado el método `AddExternalService` para aceptar un parámetro de configuración.
    
    En `appsettings.json`, se ha añadido la sección `ExternalServices` con la propiedad `BaseUrl` para el servicio de autenticación. También se ha corregido un error de formato en `AllowedHosts`.
    
    Se ha creado la clase `AuthenticationApiOptions` en `AuthenticationApiOptions.cs` para definir la estructura de configuración del servicio de autenticación.

--- Usuarios.ExternalService/DependencyInjection.cs
+++ Usuarios.ExternalService/DependencyInjection.cs
@@ -7,16 +7,22 @@ using System.Threading.Tasks;
 using Usuarios.Domain.Interfaces;
 using Microsoft.Extensions.Http;
 using Usuarios.ExternalService.Repositories;
+using Usuarios.ExternalService.Utils;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
 
 namespace Usuarios.ExternalService
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddExternalService(this IServiceCollection services)
+        public static IServiceCollection AddExternalService(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddHttpClient<IAuthenticationApiClient, AuthenticationApiClient>(client =>
+            services.Configure<AuthenticationApiOptions>(configuration.GetSection(AuthenticationApiOptions.SectionName));
+
+            services.AddHttpClient<IAuthenticationApiClient, AuthenticationApiClient>((sp,client) =>
             {
-                client.BaseAddress = new Uri("https://localhost:5019");
+                var options = sp.GetRequiredService<IOptions<AuthenticationApiOptions>>().Value;
+                client.BaseAddress = new Uri(options.BaseUrl );
             });
 
             return services;
---
+++ Usuarios.ExternalService/Utils/AuthenticationApiOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Usuarios.ExternalService.Utils
+{
+    public class AuthenticationApiOptions
+    {
+        public const string SectionName = "ExternalServices:Authentication";
+        public string BaseUrl { get; set; } = default!;
+    }
+}
